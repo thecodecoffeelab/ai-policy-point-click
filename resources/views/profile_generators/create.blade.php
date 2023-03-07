1b13a43a8bfa800df7314d9da161c42f5a586078
Mohamed NDIAYE <123922979+thecodecoffeelab@users.noreply.github.com>
2023-03-07 03:42:29 +0000

    Update create.blade.php

--- resources/views/profile_generators/create.blade.php
+++ resources/views/profile_generators/create.blade.php
@@ -106,12 +106,7 @@
                     
                     <button type="submit" class="items-center justify-center rounded-md border border-transparent bg-green-600 px-5
                      py-3 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">
-                     Generate policy 🚀
-                    </button>
-            
-                     <button type="submit" class="items-center justify-center rounded-md border border-transparent bg-black px-5
-                     py-3 text-base font-medium text-white hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
-                     Store in DB🚀
+                     Generate Policy 🚀
                     </button>
 
                 </div>
